07ca3fd48bedf4cc9c272e734b4a1f195b291cac
YIdyw <1004652005@qq.com>
2023-08-13 23:29:09 +0800

    2

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -6,8 +6,8 @@
 //
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
-
 // I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
@@ -15,7 +15,7 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -25,10 +25,14 @@ fn main() {
     }
 }
 
-pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+pub fn total_cost(item_quantity: &str) -> i32 {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>()?;
-
-    Ok(qty * cost_per_item + processing_fee)
+    let qty = item_quantity.parse::<i32>();
+    let mut res = 0;
+    match qty{
+        Ok(n) => res = n * cost_per_item + processing_fee,
+        Err(..) => println!("invalid digit found in string"),
+    };
+    res
 }
